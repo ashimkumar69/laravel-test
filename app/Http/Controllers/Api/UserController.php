1f0d3c7c9c067b9cca3f638d1d1dc1bf82e02772
ASHIM KUMAR SHAHA <ashimkumar69@gmail.com>
2022-07-12 18:01:21 +0600

    feature(Scout): User search with scout

---
+++ app/Http/Controllers/Api/UserController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use MeiliSearch\Endpoints\Indexes;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        return User::all();
+    }
+
+    public function search(Request $request)
+    {
+        $query = $request->get('query');
+
+        if (config('scout.driver') === 'meilisearch') {
+            $results = User::search($query,
+                function (Indexes $meilisearch, string $query, array $options) {
+                    $options['attributesToHighlight'] = ['name', 'email'];
+                    return $meilisearch->search($query, $options);
+                })->raw()['hits'];
+        } else {
+            $results = User::search($query)->get();
+        }
+        return $results;
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(User $user)
+    {
+    }
+
+    public function update(Request $request, User $user)
+    {
+    }
+
+    public function destroy(User $user)
+    {
+    }
+}
