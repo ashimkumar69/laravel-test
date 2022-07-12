c4e67d90ca5b6c2acf23f1784bef8bb26a51ce11
ASHIM KUMAR SHAHA <ashimkumar69@gmail.com>
2022-07-12 18:00:45 +0600

    feature(Scout): User search with scout

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::controller(UserController::class)->group(function () {
+    Route::get('users/search', 'search');
 });
+
+Route::apiResource('users', UserController::class);
+
